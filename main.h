4e76ec45359237d923ffdec33eed7222ff284c9f
rebal2023 <rebal.wehbe@gmail.com>
2023-11-26 08:55:16 -0800

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
-#ifndef PRINTF
-#define PRINTF
-
+#ifndef HEADER
+#define HEADER
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _putchar(char  c);
+int str(char *c);
 int _printf(const char *format, ...);
-
 #endif
