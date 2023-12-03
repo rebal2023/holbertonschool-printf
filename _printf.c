4bd54794212b35b2fc4308ca94e1b53a3855c7dd
rebal2023 <rebal.wehbe@gmail.com>
2023-12-03 02:45:24 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,53 +1,40 @@
 #include "main.h"
 /**
-* _printf - function
-* @format: char
-* Return: byte
+* _printf - function similar to printf
+* @format: const char *
+* Return: int
 */
-int _printf(const char *format, ...);
-unsigned int i, byte, s_count;
-va_list args;
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+	unsigned int j =0;
+        va_list args;
+        va_start(args, format);
+     
+	if (format == NULL)
+	return (-1);
 
-va_start(args, format);
-if (!format || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-_putchar(format[i]);
-}
-else if (format[i + 1] == 'c')
-{
-_putchar(va_arg(args, int));
-i++;
-}
-else if (format[i + 1] == 's')
-{
-s_count = str(va_arg(args, char *));
-i++;
-byte += (s_count - 1);
-}
-else if (format[i + 1] == '%')
-{
-_putchar('%');
-i++;
-}
-else if (format[i+1] == 'd')
-{
-i++;
-}
-else if (format[i+1] == 'i')
-{
-_putchar(va_arg(args, int));
-i++;
-}
-else if (format[i + 1])
-{
-_putchar('%');
-}
-byte += 1;
-}
-va_end(args);
-return (byte);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
+
+		else if (format[i] != '%')
+			j += _putchar(format[i]);
+		else
+		{
+			if (format[i++] == '\0')
+				return (-1);
+			else
+			{
+				if (_get_function(format[i]) != NULL)
+					j += (*_get_function(format[i]))(args);
+				else
+					j += nothing(format[i]);
+			}
+		}
+		i++;
+	}
+	va_end(args);
+	return (j);
 }
--- main.h
+++ main.h
@@ -3,7 +3,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
 int _putchar(char  c);
 int str(char *c);
 int _printf(const char *format, ...);
+int nothing(char s);
+int (*_get_function(char s))(va_list);
 #endif
