b61bf46f1f4b22326a023cd24eaa4ca54301cd94
rebal2023 <rebal.wehbe@gmail.com>
2023-12-03 03:10:48 -0800

    _printf.c

--- main.h
+++ main.h
@@ -4,13 +4,10 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <limits.h>
 
 int _putchar(char  c);
 int str(char *c);
 int _printf(const char *format, ...);
-int nothing(char s);
-int (*_get_function(char s))(va_list);
+int print_string(const char *str);
+
 #endif
---
+++ print_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_string - function to print a string
+ * @str: string to be printed
+ * Return: string
+ */
+int print_string(const char *str)
+{
+	int i = 0;
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	while (*str)
+	{
+		i += _putchar(*str);
+		str++;
+	}
+	return (i);
+}
